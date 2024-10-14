f40fd7d39616d46ce02074446a08b2705ec018ee
SilviaTGit <163752329+SilviaTGit@users.noreply.github.com>
2024-10-14 11:55:11 +0200

    Added an auth middleware to protect routes

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,18 +1,20 @@
 const express = require('express');
+const auth = require('../middleware/auth');
+
 const router = express.Router();
 const bookCtrl = require('../controllers/books');
 
 /* POST request */
-router.post('/', bookCtrl.createBook);
+router.post('/', auth, bookCtrl.createBook);
 
 /* GET request */
-router.get('/', bookCtrl.getAllBooks);
-router.get('/:id', bookCtrl.getOneBook);
+router.get('/', auth, bookCtrl.getAllBooks);
+router.get('/:id', auth, bookCtrl.getOneBook);
 
 /* PUT request */
-router.put('/:id', bookCtrl.modifyBook);
+router.put('/:id', auth, bookCtrl.modifyBook);
 
 /* DELETE request */
-router.delete('/:id', bookCtrl.deleteBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
 
   module.exports = router;
